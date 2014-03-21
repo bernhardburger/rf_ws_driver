3f826f2219bf378856c94dcd236e77c0ec8433ef
bernhardburger <bernhard.burger@doxatheca.com>
2014-03-21 09:35:37 +0100

    add

---
+++ RF_Test_Learn/src/LearnClass.java
@@ -0,0 +1,16 @@
+
+public class LearnClass {
+	public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
+	public static final String ROBOT_LIBRARY_VERSION = "0.0.1";
+	  
+	public void Helo(){
+		System.out.println("Hello");
+		
+	}
+	public void Helo_Arg(String arg){
+		System.out.println("Hello "+arg);
+	}
+	public void hs(){}
+	
+
+}
